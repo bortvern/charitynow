2afbf13c50158cceffc5a4e27a7ebc94f0c5dc9d
Bort Vern <bortvern@gmail.com>
2018-12-02 15:39:29 -0500

    fix for getting secret key from environment variable

--- cngproject/cngproject/settings.py
+++ cngproject/cngproject/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.charitynow.global']
+ALLOWED_HOSTS = ['www.charitynow.global', '127.0.0.1']
 
 
 # Application definition
